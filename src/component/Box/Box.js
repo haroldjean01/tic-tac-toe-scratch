b7660bd83eb68b3fc033184b2fe1b403f1964dde
Harold Jean <haroldjean38@yahoo.com>
2023-01-26 11:33:19 -0500

    deleted second copy of Box.js

--- src/component/Box/Box.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Square({ value, onBoxClick }) {
-  return (
-    <button className="box" onClick={onBoxClick}>
-      {value}
-    </button>
-  );
-}
