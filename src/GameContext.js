976902f12cefbaf93ac03652d2fec33f9fd1a3f3
Harold Jean <haroldjean38@yahoo.com>
2023-01-26 17:30:36 -0500

    added value state

--- src/GameContext.js
+++ src/GameContext.js
@@ -2,12 +2,23 @@ import React, { createContext, useState } from 'react';
 
 const GameContext = createContext();
 const GameProvider = ({ children }) => {
-  const [currentPlayer, setCurrentPlayer] = useState();
+  const [currentPlayer, setCurrentPlayer] = useState('X');
   const [gameMessage, setGameMessage] = useState();
   const [active, setActive] = useState();
+  const [value, setValue] = useState(null);
+
   return (
     <GameContext.Provider
-      value={{ currentPlayer, setCurrentPlayer, gameMessage, setGameMessage, active, setActive }}
+      value={{
+        currentPlayer,
+        setCurrentPlayer,
+        gameMessage,
+        setGameMessage,
+        active,
+        setActive,
+        value,
+        setValue,
+      }}
     >
       {children}
     </GameContext.Provider>
