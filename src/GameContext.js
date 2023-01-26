b630dca1b4ea5cb14d670ef6ba656cc52ebcc01d
Harold Jean <haroldjean38@yahoo.com>
2023-01-26 10:02:49 -0500

    Wrapped App.js in GameContext and added .Provider to GameProvider tag

--- src/GameContext.js
+++ src/GameContext.js
@@ -2,7 +2,7 @@ import React, { createContext } from 'react';
 
 const GameContext = createContext();
 const GameProvider = ({ children }) => {
-  return <GameProvider>{children}</GameProvider>;
+  return <GameProvider.Provider>{children}</GameProvider.Provider>;
 };
 
 export { GameContext, GameProvider };
